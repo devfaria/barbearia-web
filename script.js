1f7a8ae3b9861805cff2ec60a01fa47ce7a2427c
Luiz Faria <luizguidefaria@gmail.com>
2024-03-26 20:28:11 -0300

    Buscar Corte
    Concluído a função de buscar o corte pelo ID.

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
 const barbearia = {
-    cortes: [
-        { id: 1, tipo: "Militar", valor: 20 },
-        { id: 2, tipo: "Samurai", valor: 35 },
-        { id: 3, tipo: "Pompadour", valor: 20 },
-        { id: 4, tipo: "Moicano", valor: 10 },
-        { id: 5, tipo: "Razor part", valor: 5 },
-    ],
-  
-    barbas: [
-        { id: 1, tipo: "Capitão Jack", valor: 20 },
-        { id: 2, tipo: "Van Dyke", valor: 20 },
-        { id: 3, tipo: "Barba Média", valor: 20 },
-        { id: 4, tipo: "Barba Baixa", valor: 20 },
-        { id: 5, tipo: "Barba Zero", valor: 15 },
-    ],
-    estaAberto: true,
+  cortes: [
+    { id: 1, tipo: "Militar", valor: 20 },
+    { id: 2, tipo: "Samurai", valor: 35 },
+    { id: 3, tipo: "Pompadour", valor: 20 },
+    { id: 4, tipo: "Moicano", valor: 10 },
+    { id: 5, tipo: "Razor part", valor: 5 },
+  ],
+
+  barbas: [
+    { id: 1, tipo: "Capitão Jack", valor: 20 },
+    { id: 2, tipo: "Van Dyke", valor: 20 },
+    { id: 3, tipo: "Barba Média", valor: 20 },
+    { id: 4, tipo: "Barba Baixa", valor: 20 },
+    { id: 5, tipo: "Barba Zero", valor: 15 },
+  ],
+  estaAberto: true,
 };
-  
-  
+
 function buscaCortePorId(id) {
+  let buscarCorte = barbearia.cortes;
+  for (let i = 0; i < buscarCorte.length; i++) {
+    if (buscarCorte[i].id === id) {
+      return buscarCorte[i];
+    }
+  }
+  return "Corte não encontrado";
 }
 
-function buscaBarbaPorId(id) {
-}
+function buscaBarbaPorId(id) {}
 
-function verificaStatusBarbearia() {
-}
+function verificaStatusBarbearia() {}
 
-function retornaTodosCortes() {
-}
+function retornaTodosCortes() {}
 
-function retornaTodasBarbas() {
-}
+function retornaTodasBarbas() {}
 
-function criaPedido(nomeCliente, corteId, barbaId) {
-}
+function criaPedido(nomeCliente, corteId, barbaId) {}
 
-function atualizarServico(lista, id, valor, tipo) {
-}
+function atualizarServico(lista, id, valor, tipo) {}
 
-function calculaTotal(pedido) {
-}
-  
-  
+function calculaTotal(pedido) {}
